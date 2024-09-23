d0ffe2769d48b9b07e13f53cec94c78c00159f0c
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-23 15:36:39 +0200

    parte 1

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,7 +1,22 @@
 
-
-
 public class Main {
     public static void main(String[] args) {
+         //esercizio 1.1
+
+        int  x = 2;
+         int  y = 3;
+         int risultato =moltiplica(x,y);
+                 System.out.println("il prodotto di "+ x + " e "+ y + " è: " + risultato);
+//esercizio 1.2
+        String nome = "francesca";
+                int age = 32;
+                        System.out.println(("il nome di " + nome + " e la sua età è " + age));
+
+                        //esercizio 1.3
+        
+    }
+    //esercizio 1.1
+  static int moltiplica(int a, int b) {
+        return a * b;
     }
 }
